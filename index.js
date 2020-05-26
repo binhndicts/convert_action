325ae39201fb9634a08b619de48266c61ed2481e
BinhND <binhnd@socicom.com.vn>
2020-05-26 15:12:03 +0700

    Add addition param

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const github = require('@actions/github');
 
 try {
   const input = core.getInput('value');
+  const addition = core.getInput('addition');
   const type = core.getInput('type');
   console.log('input', input, 'type', type);
-  const result = type === 'int' ? parseInt(input) : parseFloat(input);
+  const result = type === 'int' ? parseInt(input) + parseInt(addition) : parseFloat(input) + parseFloat(addition);
   console.log('output', result);
   core.setOutput('result', result);
 } catch (error) {
