3cf44310a781ef0ba2f8d7c6c3df473664323f10
Nguyen Duc Binh <binhnd@socicom.com.vn>
2020-05-26 15:06:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const github = require('@actions/github');
 try {
   const input = core.getInput('value');
   const type = core.getInput('type');
+  console.log('input', input, 'type', type);
   const result = type === 'int' ? parseInt(input) : parseFloat(input);
+  console.log('output', result);
   core.setOutput('result', result);
 } catch (error) {
   core.setFailed(error.message);
